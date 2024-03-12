36841a9afdd2f6895ac80540b97b6752b818f76c
shreySaxena <shreyyerhs07@gmail.com>
2024-03-12 22:26:11 +0530

    spy with mockito

--- src/test/java/org/example/spy/SpyTest.java
+++ src/test/java/org/example/spy/SpyTest.java
@@ -3,6 +3,7 @@ package org.example.spy;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import java.time.LocalDate;
 
@@ -39,5 +40,31 @@ public class SpyTest {
 //        Assertions.assertEquals(1,bookRepositorySpy.timesCalled());
 
 
+    }
+
+    @Test
+    public void demoSpyWithMockito(){
+        BookRepository bookRepositorySpy= Mockito.spy(BookRepository.class);
+        BookService bookService=new BookService(bookRepositorySpy);
+
+        Book book1=new Book("1234","Mockito in Action",500, LocalDate.now()); //this will not be saved
+        Book book2=new Book("12345","JUnit 5 in Action",400, LocalDate.now());
+
+        bookService.addBook(book1);
+        bookService.addBook(book2);
+
+        // Ab tum  bookRepositoryMock.verify(book2,1); call nhi kr skte coz we are not using bookRepositoryMock class here
+
+        // To verify whether save() was called for book2 using mockito is this
+        Mockito.verify(bookRepositorySpy).save(book2);
+
+        // This will give an exception kiuki book1 ke saath verify call hua hi nhi tha
+        Mockito.verify(bookRepositorySpy).save(book1);
+
+        // This will test whether save(book2) was called once or how many times, since it was called once so this case will pass
+        Mockito.verify(bookRepositorySpy,Mockito.times(1)).save(book2);
+
+        // This will test whether save(book1) was called zero times or how many times, since it was not called  so this case will pass
+        Mockito.verify(bookRepositorySpy,Mockito.times(0)).save(book1);
     }
 }
